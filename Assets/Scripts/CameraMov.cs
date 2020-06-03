4f57ad1f0d85dbc70ce303e6bda46ab7afabb783
Gasthor <64218013+Gasthor@users.noreply.github.com>
2020-06-03 16:06:41 -0400

    Test

---
+++ Assets/Scripts/CameraMov.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMov : MonoBehaviour
+{
+    public Vector3 cam;
+    private void FixedUpdate()
+    {
+        Vector3 pos = transform.position;
+        pos.x = cam.x;
+        pos.y = cam.y;
+        pos.z = cam.z;
+        transform.position = pos;
+    }
+}
+
---
+++ Assets/Scripts/ChangeRoom.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeRoom : MonoBehaviour
+{
+    public Vector3 cameraChange;
+    public Vector3 playerChange;
+    private CameraMov cam;
+    void Start()
+    {
+        cam = Camera.main.GetComponent<CameraMov>();
+    }
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            cam.cam += cameraChange;
+            collision.transform.position += playerChange;
+        }
+    }
+}
