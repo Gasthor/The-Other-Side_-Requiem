7ec8bafafbbc6747eda94ca4e59ac48ad3132bf1
seba6453 <sebastian.astudillo@alumnos.ucn.cl>
2020-07-10 17:44:04 -0400

    0.1.1
    creacion del menu de inicio y pausa

---
+++ Assets/Scripts/Menu/Setting.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Setting : MonoBehaviour
+{
+    public static bool pausa = false;
+
+    public GameObject pauseMenu;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pausa)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        pausa = true;
+    }
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        pausa = false;
+    }
+    public void Inicio(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
