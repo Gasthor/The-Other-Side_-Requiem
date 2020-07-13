c35aaa31175132aeabba5cd3b7992e915a5ca261
zSouLKy <felipe.valenzuela@gmail.com>
2020-07-12 21:16:46 -0400

    ghgfh

--- Assets/Scripts/Enemy/Enem.cs
+++ Assets/Scripts/Enemy/Enem.cs
@@ -2,21 +2,89 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enem : MonoBehaviour { 
-    public int health;
-    public string enemyName;
-    public int baseAttack;
-    public float moveSpeed;
+public class Enem : MonoBehaviour {
+    public float visionRadio;
+    public float attackRadio;
+    public float speed;
+    bool attacking;
+    public float attackSpeed;
+    public Transform homePosition;
+    public GameObject Objeto;
 
-    // Start is called before the first frame update
+    GameObject player;
+    Vector3 initialPosition;
+    Rigidbody2D rb2d;
+
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        initialPosition = transform.position;
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 target = initialPosition;
+        if (player != null)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(
+                transform.position,
+                player.transform.position - transform.position,
+                visionRadio,
+                1 << LayerMask.NameToLayer("Default"));
+
+
+            Vector3 forward = transform.TransformDirection(player.transform.position - transform.position); // debug??
+            Debug.DrawRay(transform.position, forward, Color.red);// debug
+
+            if (hit.collider != null)
+            {
+                if (hit.collider.tag == "Player")
+                {
+                    target = player.transform.position;
+                }
+            }
+
+            float distance = Vector3.Distance(target, transform.position);
+            Vector3 dir = (target - transform.position).normalized;
+
+            if (target != initialPosition && distance < attackRadio)
+            {
+                //animacion de ataque xd
+                if (!attacking) StartCoroutine(Attack(attackSpeed));
+            }
+            else
+            {
+                //transform.position = Vector3.MoveTowards(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
+                rb2d.MovePosition(transform.position + dir * speed * Time.deltaTime);
+                // animacion de persecucion xd
+            }
+
+            if (target == initialPosition && distance < 0.02f)
+            {
+                transform.position = initialPosition;
+            }
+            Debug.DrawLine(transform.position, target, Color.green);// debug
+        }
+    }
+
+    IEnumerator Attack(float seconds)
+    {
+        attacking = true;
+
+        if (player != homePosition && Objeto != null)
+        {
+            Instantiate(Objeto, transform.position, transform.rotation);
+            yield return new WaitForSeconds(seconds);
+        }
+        attacking = false;
+    }
+    // debug
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, visionRadio);
+        Gizmos.DrawWireSphere(transform.position, attackRadio);
     }
 }
---
+++ Assets/Scripts/Enemy/attackEnemy.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attackEnemy : MonoBehaviour
+{
+    //esta clase sirve para cualquier ataque parecido.
+    public float speed;
+
+    PlayerMovement pl;
+    GameObject PlayerMovement;
+    Rigidbody2D rb2d;
+
+    Vector3 target, dir;
+
+    void Start()
+    {
+        PlayerMovement = GameObject.FindGameObjectWithTag("Player");
+        rb2d = GetComponent<Rigidbody2D>();
+
+
+        if (PlayerMovement != null)
+        {
+            target = PlayerMovement.transform.position;
+            dir = (target - transform.position).normalized;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (target != Vector3.zero)
+        {
+            rb2d.MovePosition(transform.position + (dir * speed) * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.transform.tag == "Player" || col.transform.tag == "Player2" || col.transform.tag == "Attack") // ojo con los tag!!!!
+        {
+            if (col.transform.tag == "Player" || col.transform.tag == "Player2") col.SendMessage("TakeDamage", 10);
+            Destroy(gameObject);
+
+        }
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
