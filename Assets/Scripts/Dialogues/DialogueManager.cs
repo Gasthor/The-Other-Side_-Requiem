a6d324700be567c4425e0d43442b3ffa10c8701e
ip7cr7i <ignaciopay2001@gmail.com>
2020-07-20 20:16:48 -0400

    0.1.18
    Dialogues

---
+++ Assets/Scripts/Dialogues/DialogueManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public GameObject dBox;
+    public Text dText;
+
+    public bool dialogueActive;
+
+    public string[] dialogLines;
+    public int currentLine;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(dialogueActive && Input.GetKeyDown(KeyCode.E))
+        {
+            /*dBox.SetActive(false);
+            dialogueActive = false;*/
+
+            currentLine++;
+        }
+        if(currentLine >= dialogLines.Length)
+        {
+            dBox.SetActive(false);
+            dialogueActive = false;
+
+            currentLine = 0;
+        }
+
+        dText.text = dialogLines[currentLine];
+    }
+    public void ShowBox(string dialogue)
+    {
+        dialogueActive = true;
+        dBox.SetActive(true);
+        dText.text = dialogue;
+    }
+
+    public void ShowDialogue()
+    {
+        dialogueActive = true;
+        dBox.SetActive(true);
+    }
+}
