728a930ca8d4d897d4be343c3363a5c39985e2da
Gasthor <64218013+Gasthor@users.noreply.github.com>
2020-08-31 14:25:29 -0400

    creditos fixed

--- Assets/Scenes/Creditos/credits.cs
+++ Assets/Scenes/Creditos/credits.cs
@@ -3,30 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class credits : MonoBehaviour
 {
-    void Start()
-    {
-
-        GameObject camera = GameObject.Find("Main Camera");
-
-        var videoPlayer = camera.AddComponent<UnityEngine.Video.VideoPlayer>();
-
-        videoPlayer.playOnAwake = false;
-
-        videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.CameraNearPlane;
-
-        videoPlayer.url = "Assets/Scenes/Creditos/creditosFinalxd.mp4";
-
-        videoPlayer.isLooping = true;
-
-        videoPlayer.loopPointReached += EndReached;
-
-        videoPlayer.Play();
-    }
-
-    void EndReached(UnityEngine.Video.VideoPlayer vp)
-    {
-        vp.playbackSpeed = vp.playbackSpeed / 10.0F;
-    }
+   
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
